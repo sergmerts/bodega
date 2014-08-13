13032f056d9269a4982929643cac12e9fb1e9286
Sergii Mertsalov & Nick Bernard <student@epicodus.com>
2014-08-13 10:37:19 -0700

    list cashiers

--- pos.rb
+++ pos.rb
@@ -26,13 +26,15 @@ def manager_menu
   until choice == '0'
     puts "1: Add product"
     puts "2: List products"
-    puts "3: Add Cashier"
+    puts "3: Add cashier"
+    puts "4: List cashiers"
     puts "0: Leave the store"
     choice = gets.chomp
     case choice
     when '1' then add_product
     when '2' then list_products
     when '3' then add_cashier
+    when '4' then list_cashiers
     when '0' then exit
     else
       puts "Not a valid option."
@@ -47,6 +49,7 @@ def add_product
   price = gets.chomp
   new_product = Product.create({name: name, price: price})
   puts "#{new_product.name} added!"
+  puts "\n\n"
 end
 
 def list_products
@@ -54,6 +57,7 @@ def list_products
   Product.all.each_with_index do |product, i|
     puts "#{i + 1}. #{product.name}: $#{product.price}"
   end
+  puts "\n\n"
 end
 
 def add_cashier
@@ -61,6 +65,16 @@ def add_cashier
   name = gets.chomp
   new_cashier = Cashier.create({name: name, loged_in: false})
   puts "#{new_cashier.name} added!"
+  puts "\n\n"
 end
+
+def list_cashiers
+  puts "Cashiers: "
+  Cashier.all.each_with_index do |cashier, i|
+    puts "#{i + 1}. #{cashier.name}"
+  end
+  puts "\n\n"
+end
+
 
 welcome
